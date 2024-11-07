baea5b99fe7deb903df7ad9416741f0826376cb1
ssuisig <jsy6430000@naver.com>
2024-11-07 11:38:10 +0900

    commit1

---
+++ Session_standardLogin/src/main/java/ssui/session_standardlogin/SessionStandardLoginApplication.java
@@ -0,0 +1,13 @@
+package ssui.session_standardlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionStandardLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionStandardLoginApplication.class, args);
+	}
+
+}
---
+++ Session_standardLogin/src/test/java/ssui/session_standardlogin/SessionStandardLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package ssui.session_standardlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionStandardLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
