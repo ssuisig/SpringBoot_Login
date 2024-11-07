a5d6436e68aa7351cef38f759a3c25239bef108d
ssuisig <jsy6430000@naver.com>
2024-11-07 13:32:28 +0900

    commit2

---
+++ Session_standardLogin/src/main/java/ssui/session_standardlogin/controller/LoginController.java
@@ -0,0 +1,14 @@
+package ssui.session_standardlogin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginP() {
+
+        return "login";
+    }
+}
