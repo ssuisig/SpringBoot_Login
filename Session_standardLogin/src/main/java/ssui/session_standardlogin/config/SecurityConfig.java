9fcaefd9f601706a5b2ab6d0df45fe4f3741b77e
ssuisig <jsy6430000@naver.com>
2024-11-09 16:30:53 +0900

    join_page

--- Session_standardLogin/src/main/java/ssui/session_standardlogin/config/SecurityConfig.java
+++ Session_standardLogin/src/main/java/ssui/session_standardlogin/config/SecurityConfig.java
@@ -4,12 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // 단방향 해시 암호화
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
